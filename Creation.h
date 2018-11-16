1281666d4a37c22e4aec44a4d30bd10e6acdb02e
tomaszrogala <41085541+tomaszrogala@users.noreply.github.com>
2018-11-16 20:35:38 +0100

    Update Creation.h

--- Creation.h
+++ Creation.h
@@ -10,7 +10,7 @@ public:
 	virtual void change(bool &flag, char*M[]) = 0;
 	virtual void view() = 0; 
 	virtual void erase(bool *C[]) = 0;
-	virtual void move(char &button, char *M[], bool &change, int p_x, int p_y, int &c_numb, bool *C[]) = 0;
+	virtual void move(char &button, char *M[], bool &change, int p_x, int p_y, int &c_numb, bool *C[], bool &next) = 0;
 	virtual bool die(char *M[], int x, int y, bool ghost_death) = 0;
 
 protected:
