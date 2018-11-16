6346d2a19dba41e88159f2ac055d5db680b28af3
tomaszrogala <41085541+tomaszrogala@users.noreply.github.com>
2018-11-16 20:36:10 +0100

    Update Ghost.h

--- Ghost.h
+++ Ghost.h
@@ -11,7 +11,7 @@ public:
 	virtual void view() = 0;
 	virtual void erase(bool *C[]) = 0;
 	virtual void change(bool &flag, char*M[]) = 0;
-	virtual void move(char &button, char *M[], bool &change, int p_x, int p_y, int &c_numb, bool *C[]) = 0;
+	virtual void move(char &button, char *M[], bool &change, int p_x, int p_y, int &c_numb, bool *C[], bool &next) = 0;
 	virtual void search_path(char *M[], bool &change, int x, int y) = 0;
 	virtual void creat_path(char *M[]) = 0; 
 	virtual bool die(char *M[], int x, int y, bool ghost_death) = 0;
